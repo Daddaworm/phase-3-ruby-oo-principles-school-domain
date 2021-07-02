efada13fa9de4c633272cc4be8073e567e3bf6c8
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-02 16:25:54 -0400

    commit tree 372178c3c46e806cf1417f9203ccd70549f865d5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+
+
+class School
+    attr_accessor :name, :roster
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+    
+    
+    def grade(grade)
+        # binding.pry
+        roster[grade]
+        
+    end
+
+
+    def sort
+        sorted_hash = {}
+        roster.each do |grade, name|
+            binding.pry
+            sorted_hash[grade] = name.sort
+        end 
+        sorted_hash
+    end
+
+
+end
